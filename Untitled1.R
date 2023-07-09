1f44dc97d1f7361c865827e714e495dacd2291b5
Alef Jonathan <a.lef93@hotmail.com>
2023-07-08 23:17:57 -0300

    comit

---
+++ Untitled1.R
@@ -0,0 +1,7 @@
+usethis::use_git_config(
+  user.name="alefjonathan29",
+  user.email="a.lef93@hotmai.com"
+)
+usethis::create_github_token()
+usethis::edit_r_environ()
+#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
