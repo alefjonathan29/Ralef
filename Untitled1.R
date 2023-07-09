4a3c6f6da106a5831d886d2b1bc2e2a0a50069b9
Alef Jonathan <a.lef93@hotmail.com>
2023-07-08 23:22:42 -0300

    Vaiiii

--- Untitled1.R
+++ Untitled1.R
@@ -1,7 +1,8 @@
 usethis::use_git_config(
-  user.name="alefjonathan29",
+  user.name="Alef Jonathan",
   user.email="a.lef93@hotmai.com"
 )
 usethis::create_github_token()
 usethis::edit_r_environ()
 #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
+usethis::git_sitrep()
