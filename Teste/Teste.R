8b39d5992ad143ee27ee1bcdc72348222cfb92e6
Alef Jonathan <a.lef93@hotmail.com>
2023-07-09 14:12:04 -0300

    Atualizando arquivo de teste

---
+++ Teste/Teste.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(rstatix)
+library(car)
+library(tidyverse)
+
+devtools::install_github("alefjonathan29/Ralef", force = T)
+
+library(Ralef)
+
+testar_Ralef()
+criar_interface(iris)
