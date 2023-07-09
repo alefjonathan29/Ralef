1b6e5e16d66b52b9d635aeb0f842f802f00b37c0
Alef Jonathan <a.lef93@hotmail.com>
2023-07-09 00:15:35 -0300

    Novo

--- Untitled1.R
+++
@@ -1,8 +0,0 @@
-usethis::use_git_config(
-  user.name="Alef Jonathan",
-  user.email="a.lef93@hotmai.com"
-)
-usethis::create_github_token()
-usethis::edit_r_environ()
-#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
-usethis::git_sitrep()
