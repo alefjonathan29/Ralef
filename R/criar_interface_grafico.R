cbf018a554df512e8e4e6e0a07521fd6d4ad7d11
Alef Jonathan <a.lef93@hotmail.com>
2023-07-10 04:25:11 -0300

    Atualizando o Facet

--- R/criar_interface_grafico.R
+++ R/criar_interface_grafico.R
@@ -13,10 +13,10 @@
 #'
 #' @export
 criar_interface_grafico <- function(dados) {
-  #Verificando pacotes
-  pacotes <- c("shiny", "rstatix", "car", "tidyverse","ggplot2")                                        # Specify your packages
-  nao_instalados <- pacotes[!(pacotes %in% installed.packages()[ , "Package"])]    # Extract not installed packages
-  if(length(nao_instalados)) install.packages(nao_instalados)                               # Install not installed packages
+  # Verificando pacotes
+  pacotes <- c("shiny", "rstatix", "car", "tidyverse","ggplot2","ggthemes")
+  nao_instalados <- pacotes[!(pacotes %in% installed.packages()[ , "Package"])]
+  if(length(nao_instalados)) install.packages(nao_instalados)
   lapply(pacotes, require, character.only = TRUE)
 
   # Define a interface gráfica
@@ -25,15 +25,15 @@ criar_interface_grafico <- function(dados) {
     sidebarLayout(
       sidebarPanel(
         selectInput("variavel_dependente", "Variável Dependente:",
-                    choices = names(dados)),
+                    choices = c("", names(dados))),
         selectInput("grupo", "Categorias (Variável independente):",
-                    choices = names(dados)),
-        selectInput("faceta", "Variável facet",
-                    choices = names(dados))
+                    choices = c("", names(dados))),
+        selectInput("faceta", "Variável facet:",
+                    choices = c("Nenhum", names(dados)))
       ),
       mainPanel(
         plotOutput("boxplot"),
-        verbatimTextOutput("codigo_grafico")  # Saída para exibir o código do gráfico
+        textOutput('codigo_grafico')  # Saída para exibir o código do gráfico
       )
     )
   )
@@ -43,25 +43,50 @@ criar_interface_grafico <- function(dados) {
 
     # Renderizar o boxplot na interface
     output$boxplot <- renderPlot({
+      if (is.null(input$variavel_dependente) || is.null(input$grupo) || is.null(input$faceta)) {
+        return(NULL)
+      }
+
       variavel_dependente <- dados[[input$variavel_dependente]]
       grupo <- dados[[input$grupo]]
+      faceta1 <- input$faceta
       faceta <- dados[[input$faceta]]
-      dados_plot <- data.frame(grupo, variavel_dependente, faceta)
-      ggplot(dados_plot, aes(x = grupo, y = variavel_dependente, fill = grupo)) +
-        geom_boxplot() +
-        theme_bw() +
-        facet_wrap(vars(faceta))
+
+      if (faceta1 == "Nenhum") {
+        dados_plot <- data.frame(grupo, variavel_dependente)
+        ggplot(dados_plot, aes(x = grupo, y = variavel_dependente, fill = grupo)) +
+          geom_boxplot()+
+          theme_bw()
+
+      } else {
+        dados_plot <- data.frame(grupo, variavel_dependente, faceta)
+        ggplot(dados_plot, aes(x = grupo, y = variavel_dependente, fill = grupo)) +
+          geom_boxplot() +
+          facet_wrap(vars(faceta))+
+          theme_bw()
+      }
     })
 
     # Renderizar o código do gráfico na interface
     output$codigo_grafico <- renderPrint({
+      if (is.null(input$variavel_dependente) || is.null(input$grupo) || is.null(input$faceta)) {
+        return(NULL)
+      }
+
       variavel_dependente <- input$variavel_dependente
       grupo <- input$grupo
       faceta <- input$faceta
-      code <- paste0("ggplot(dados_plot, aes(x = ", grupo, ", y = ", variavel_dependente, ", fill = ", grupo, ")) +\n",
-                     "  geom_boxplot() +\n",
-                     "  theme_bw() +\n",
-                     "  facet_wrap(vars(", faceta, "))")
+
+      if (faceta == "Nenhum") {
+        code <- paste0("ggplot(dados_plot, aes(x = ", grupo, ", y = ", variavel_dependente, ", fill = ", grupo, ")) +",
+                       "  geom_boxplot() +",
+                       "  theme_bw()")
+      } else {
+        code <- paste0("ggplot(dados_plot, aes(x = ", grupo, ", y = ", variavel_dependente, ", fill = ", grupo, ")) +",
+                       "  geom_boxplot() +",
+                       "  theme_bw() +",
+                       "  facet_wrap(vars(", faceta, "))")
+      }
       code
     })
   }
